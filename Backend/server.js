becf6dec97cdb6c375afd872c36ab6df1d3f2624
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-04-18 13:57:05 +0530

    Basic Setup Completed

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { YSocketIO } from "y-socket.io/dist/server";
+
+const app = express();
+const httpServer = createServer(app);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello World!",
+    success: true,
+  });
+});
+
+app.get("/heatlh", (req, res) => {
+  res.status(200).json({
+    message: "ok",
+    success: true,
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
